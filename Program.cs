1cc47c377fc3375caf6d2e46001c4ac32b55c31a
Victor Shi <victorshi29@gmail.com>
2021-11-04 17:01:06 -0400

    test

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace twitchchatbot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string uri = "irc.chat.twitch.tv";
+            string password = "oauth:c7lmtmebed84ctn5fpbhtbppbt69bc";
+            string botUsername = "pockychubot";
+            int port = 6667;
+            string line = "";
+            string[] split;
+            string message;
+            string sender;
+
+            /*split = ":tmi.twitch.tv 002 pockychubot :Your host is tmi.twitch.tv".Split(" ");
+            foreach(string str in split)
+            {
+                Console.WriteLine(str);
+            }
+            */
+            
+            var tcpClient = new System.Net.Sockets.TcpClient();
+            await tcpClient.ConnectAsync(uri, port);
+            var streamWriter = new System.IO.StreamWriter(tcpClient.GetStream()) { NewLine = "\r\n", AutoFlush = true};
+            var streamReader = new System.IO.StreamReader(tcpClient.GetStream());
+
+            await streamWriter.WriteLineAsync($"PASS {password}");
+            await streamWriter.WriteLineAsync($"NICK {botUsername}");
+            await streamWriter.WriteLineAsync($"JOIN #morenaptime");
+            await streamWriter.WriteLineAsync("PRIVMSG #morenaptime :This is a test message");
+            
+            while (true)
+            {
+                line = await streamReader.ReadLineAsync();
+                Console.WriteLine(line);
+                split = line.Split(" ");
+
+                //PING :tmi.twitch.tv
+                //Respond with PONG :tmi.twitch.tv
+                if (line.StartsWith("PING"))
+                {
+                    Console.WriteLine("PING");
+                    await streamWriter.WriteLineAsync($"PONG {split[1]}");
+                }
+                if (split[1].Equals("PRIVMSG"))
+                {
+                    sender = split[2].Substring(1);
+                    message = split[3].Substring(1);
+                    Console.WriteLine(message);
+                    if (message.Equals("!pika"))
+                    {
+                        string pika = "⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿ ⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿ ⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿ ⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿";
+                        await streamWriter.WriteLineAsync($"PRIVMSG #morenaptime : {pika}");
+                    }
+                }
+            }
+        }
+    }
+}
